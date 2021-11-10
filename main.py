53f3305c0500287900c1b3d316f390f807d642f6
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-10 22:57:55 +0900

    changed reply Message

--- main.py
+++ main.py
@@ -52,10 +52,14 @@ def callback():
 # 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event == "アカネチャンの使い方":
+    if event.message.text == "アカネチャンの使い方":
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='好きな文字をいれてね'))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='あほくさ'))
 
 # ポート番号の設定
 if __name__ == "__main__":
