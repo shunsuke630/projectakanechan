692f7f09cb388c87e50cb067ffff1cfafdf1f1d9
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-13 00:33:29 +0900

    fix

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ def make_image_message():
 def make_image_message1():
     
     messages = ImageSendMessage(
-        original_content_url = 'https://i.gyazo.com/606a3b329d0b247e1667fd998e2f3545.jpg', #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-        preview_image_url = 'https://i.gyazo.com/606a3b329d0b247e1667fd998e2f3545.jpg' #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        original_content_url = 'https://i.gyazo.com/e54023898ac2a8d9902a4850a2a44bf0.jpg', #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url = 'https://i.gyazo.com/e54023898ac2a8d9902a4850a2a44bf0.jpg' #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
     )
     return messages
 
