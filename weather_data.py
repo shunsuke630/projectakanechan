3438e8bc30538d9b4cb6b6dc14fe1f1094411745
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-12 22:37:11 +0900

    fix

--- weather_data.py
+++ weather_data.py
@@ -6,12 +6,15 @@ urls = ['https://tenki.jp/forecast/3/13/4210/10203/',
        'https://tenki.jp/forecast/3/11/4020/8219/'
 ]
 def get_weather():
+    weathers = []
     for url in urls:
         res = requests.get(url)
         html = BeautifulSoup(res.text, 'html.parser')
         title = html.find('h2').get_text().split('の')
         weather = html.find('p',{'class': 'weather-telop'}).get_text()
-        weather = '\n'.join([title[0], weather])
-    return weather
+        weather = [title[0],weather]
+        weathers.append(weather)
+
+    return weathers
 
 
