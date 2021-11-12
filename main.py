5da0895246a6a42e4971791dde9046e2e0e64141
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-13 00:31:49 +0900

    fix

--- main.py
+++ main.py
@@ -83,7 +83,11 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             image_data)
-
+    elif event.message.text == "シフォンケーキ":
+        image_data = make_image_message1()
+        line_bot_api.reply_message(
+            event.reply_token,
+            image_data)
 
 def make_image_message():
     
@@ -92,6 +96,15 @@ def make_image_message():
         preview_image_url = 'https://i.gyazo.com/0aff0fbedd9286058065c158187cedb2.jpg' #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
     )
     return messages
+
+def make_image_message1():
+    
+    messages = ImageSendMessage(
+        original_content_url = 'https://i.gyazo.com/606a3b329d0b247e1667fd998e2f3545.jpg', #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url = 'https://i.gyazo.com/606a3b329d0b247e1667fd998e2f3545.jpg' #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+    )
+    return messages
+
 
 
 # ポート番号の設定
