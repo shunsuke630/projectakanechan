ced67fa53c0c6b2f19377e625d4d84f2a2da288e
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-12 22:07:14 +0900

    fix

--- weather_data.py
+++ weather_data.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://tenki.jp/forecast/3/13/4210/10203/'
-
-
+urls = ['https://tenki.jp/forecast/3/13/4210/10203/',
+       'https://tenki.jp/forecast/3/13/4210/10201/',
+       'https://tenki.jp/forecast/3/11/4020/8219/'
+]
 def get_weather():
-    res = requests.get(url)
-    html = BeautifulSoup(res.text, 'html.parser')
-    title = html.find('h2').get_text().split('の')
-    weather = html.find('p',{'class': 'weather-telop'}).get_text()
-    weather = [title[0], weather]
-    return weather
+    for url in urls:
+        res = requests.get(url)
+        html = BeautifulSoup(res.text, 'html.parser')
+        title = html.find('h2').get_text().split('の')
+        weather = html.find('p',{'class': 'weather-telop'}).get_text()
+        weather = '\n'.join([title[0], weather])
+        return weather
 
 
