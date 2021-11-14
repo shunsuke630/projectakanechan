c5b5f29d1fa70db37aa39a5fda64764f87adfb39
Shunsuke Sugimoto <shunsuke6300@gmail.com>
2021-11-14 21:46:43 +0900

    fix images.py

--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 import random
 from linebot.models import ImageSendMessage
 
-horror = ['https://i.gyazo.com/e54023898ac2a8d9902a4850a2a44bf0.jpg']
+horror = ['https://i.gyazo.com/e54023898ac2a8d9902a4850a2a44bf0.jpg', 'https://i.gyazo.com/ba9501bd876e77c5d67332e6630a81c2.jpg', 'https://i.gyazo.com/606a3b329d0b247e1667fd998e2f3545.jpg']
 
 def horror_image_message():
     url =random.choice(horror)
